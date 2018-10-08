1d12f9b8c44c4333d03c2044f0ec8f43d365ed0d
Wesarut <anubis.smile@gmail.com>
2018-10-08 09:56:12 +0700

    Initial Commit

---
+++ MYIOSDEV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYIOSDEV
+//
+//  Created by macbookair on 8/10/2561 BE.
+//  Copyright © 2561 smartict. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
