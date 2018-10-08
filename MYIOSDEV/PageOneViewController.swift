7bd5d0a18c8a94da5bd2f430001c5361d51c6803
Wesarut <anubis.smile@gmail.com>
2018-10-08 16:12:40 +0700

    Finished first menu - open web view and open web pdf by internal resource path.

--- MYIOSDEV/PageOneViewController.swift
+++ MYIOSDEV/PageOneViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 import WebKit
 
-class PageOneViewController: UIViewController {
+class PageOneViewController: UIViewController, WKNavigationDelegate {
     
     @IBOutlet weak var mWebKit: WKWebView!
     @IBOutlet weak var mLoading: UIActivityIndicatorView!
-
+    
+    let mURL = "http://www.codemobiles.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        //Important
+        self.mWebKit.navigationDelegate = self
+        self.openWeb()
     }
     
     @IBAction func onSegmentChange(sender: UISegmentedControl){
@@ -25,19 +29,45 @@ class PageOneViewController: UIViewController {
         switch sender.selectedSegmentIndex {
         case 0:
             print("select index 0")
+            self.openWeb()
         default:
             print("select index 1")
+            self.openPDF()
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func openPDF(){
+        let pdfPath = Bundle.main.path(forResource: "product", ofType: "pdf")
+        //let pdfPath = Bundle.main.path(forResource: "product.pdf", ofType: nil)
+        
+        let pdfData = NSData(contentsOfFile: pdfPath!)
+        
+        self.mWebKit.load(pdfData! as Data, mimeType: "application/pdf", characterEncodingName: "utf-8", baseURL: NSURL() as URL)
     }
-    */
-
+    
+    func openWeb() {
+        let url = URL(string: self.mURL)
+        let request = URLRequest(url: url!)
+        
+        self.mWebKit.load(request)
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        self.mLoading.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.mLoading.stopAnimating()
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
